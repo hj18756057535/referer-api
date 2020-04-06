2f54fd65142be6fd8102b2fb49b3e927a1fb36ad
hj18756057535 <956126200@qq.com>
2020-04-06 11:51:17 +0800

    init: project init

---
+++ src/main/java/com/hj/baidu/RefererApiApplication.java
@@ -0,0 +1,13 @@
+package com.hj.baidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefererApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefererApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hj/baidu/RefererApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hj.baidu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefererApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
